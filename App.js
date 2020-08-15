c792e3abc1d2f5141fa825c59ae83deaffb5c3e2
Aashish Kushwaha <aashishk1997@gmail.com>
2020-08-16 01:20:40 +0530

    added custom font, custom BodyText component, default styles, local/network image

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState(0);
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    // startAsync takes function & function should return Promise
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(error) => console.log(error)}
+      />
+    );
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
@@ -25,6 +46,14 @@ export default function App() {
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
+  content = (
+    <GameOverScreen
+      numberOfRounds={2}
+      userNumber={7}
+      onRestartGame={configureNewGameHandler}
+    />
+  );
+
   if (userNumber && guessRounds <= 0)
     content = (
       <GameScreen userChoise={userNumber} onGameOver={gameOverHandler} />
